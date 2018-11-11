5e4b2f97121d6b6c169b52094d1cbe125b4c9492
Julen Pérez Hernández <i72pehej@uco.es>
2018-11-11 15:01:36 +0100

    Dudas Jugador.cc

--- POO/p2/jugador.cc
+++ POO/p2/jugador.cc
@@ -6,24 +6,13 @@
 void Jugador::setApuestas(){
   getApuestas().clear();
 
+  std::string apuestaFichero;
   std::string nomFichero = getDNI() + ".txt";
-
   std::ifstream fichero(nomFichero);
 
-
+  while (std::getline(fichero, apuestaFichero, '\n')) { // Bucle while para leer los elementos del fichero.
+    apuestas_.push_back(apuestaFichero); // Introduce cada elemento de uno en uno en la lista de apuestas.
+  }
 
   fichero.close();
-}
-
-
-
-
-
-
-
-
-
-int main(int argc, char const *argv[]) {
-  /* code */
-  return 0;
 }
