73dae699752e8afc8f98b6a239ba1d0f1b4f2b8c
Julen Pérez Hernández <i72pehej@uco.es>
2018-11-11 22:40:16 +0100

    Commit Domingo2

--- POO/p2/jugador.cc
+++ POO/p2/jugador.cc
@@ -10,9 +10,14 @@ void Jugador::setApuestas(){
   std::string nomFichero = getDNI() + ".txt";
   std::ifstream fichero(nomFichero);
 
+while (std::getline(fichero, apuestaFichero)) {
+  apuestas_.push_back(apuestaFichero);
+}
+
+/*
   while (std::getline(fichero, apuestaFichero, '\n')) { // Bucle while para leer los elementos del fichero.
     apuestas_.push_back(apuestaFichero); // Introduce cada elemento de uno en uno en la lista de apuestas.
   }
-
+*/
   fichero.close();
 }
