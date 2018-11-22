5ada1a6c543d2c9144fb214a191df8fc82efb46e
Julen Pérez Hernández <43370446+i72pehej@users.noreply.github.com>
2018-11-22 10:43:57 +0100

    Add files via upload
    Ejercicio3_1 SO -> 1Productor - 1Consumidor

--- SO/Practica2/Ejercicio3_1.c
+++ SO/Practica2/Ejercicio3_1.c
@@ -4,29 +4,37 @@
 #include <stdlib.h>
 #include <time.h>
 
-double counter = 0;
+double suma_P = 0, suma_C = 0;
 
-//Corrección con Semáforos.
-pthread_mutex_t semaforo;
+//Inicialización de Semáforos.
+pthread_mutex_t semaforo_P, semaforo_C;
 
-#define ITER	10
-#define NHILOS	4
+#define N 30
+
+//Buffer/Vector de almacenamiento compartido.
+int buffer[N];
 
 int main()
 {
     pthread_t hilo_P, hilo_C;
     int status, i, idProductor = 1, idConsumidor = 2;
-    extern double counter;
-    void *adder(void *);
-    double *r_value;
+    extern double suma_P, suma_C;
+    void *productor(void *);
+    void *consumidor(void *);
+    double *r_value_P, *r_value_C;
 
     srand(time(NULL));
 
     //Creación del semáforo.
-    if(pthread_mutex_init(&semaforo, NULL) != 0){
-      printf("\nERROR. No se pudo crear el semáforo correctamente.\n");
+    if(pthread_mutex_init(&semaforo_P, NULL) != 0){
+      printf("\nERROR. No se pudo crear el semáforo productor correctamente.\n");
       return 1;
-    }else{printf("\nEl semáforo se ha creado correctamente.\n");}
+    }else{printf("\nEl semáforo productor se ha creado correctamente.\n");}
+
+    if(pthread_mutex_init(&semaforo_C, NULL) != 0){
+      printf("\nERROR. No se pudo crear el semáforo consumidor correctamente.\n");
+      return 1;
+    }else{printf("\nEl semáforo consumidor se ha creado correctamente.\n");}
 
     //Creación de un hilo productor.
 	  if ((status = pthread_create(&hilo_P, NULL, productor, (void *) &idProductor)))
@@ -37,85 +45,92 @@ int main()
     exit(status);
 
     // Wait threads
-    for (i = 0; i < NHILOS; i++) {
-	     pthread_join(hilos[i], (void **) &r_value);
-	     printf("Value returned by %lu thread: %lf\n", hilos[i], *r_value);
-    }
+    pthread_join(hilo_P, (void **) &r_value_P);
+    printf("Value returned by %lu thread: %lf\n", hilo_P, *r_value_P);
+    pthread_join(hilo_C, (void **) &r_value_C);
+    printf("Value returned by %lu thread: %lf\n", hilo_C, *r_value_C);
 
-    //Destrucción del Semáforo.
-    if (pthread_mutex_destroy(&semaforo) != 0) {
-      printf("\nERROR. No se pudo destruir el semáforo.\n");
+    //Destrucción de Semáforos.
+    if (pthread_mutex_destroy(&semaforo_P) != 0) {
+      printf("\nERROR. No se pudo destruir el semáforo productor.\n");
       return 1;
     }
-    else{printf("\nEl semáforo se ha destruido. Memoria liberada.\n");}
+    else{printf("\nEl semáforo productor se ha destruido. Memoria liberada.\n");}
+
+    if (pthread_mutex_destroy(&semaforo_C) != 0) {
+      printf("\nERROR. No se pudo destruir el semáforo consumidor.\n");
+      return 1;
+    }
+    else{printf("\nEl semáforo consumidor se ha destruido. Memoria liberada.\n");}
 
     // Final result
-    fprintf(stdout, "\nResultado final: %f\n", counter);
+    fprintf(stdout, "\nResultado final productor: %f\n", suma_P);
+    fprintf(stdout, "Resultado final consumidor: %f\n\n", suma_C);
 
     return 0;
 }
 
+//Hilo productor. -> Genera números de 0 a 1000 y los introduce en el buffer.
 void *productor(void *p)
 {
-    double l, *to_return;
-    extern double counter;
+    double l, *to_return_P;
+    extern double suma_P;
+    extern int buffer[N];
     int *id, i;
 
     id = (int *) p;
 
-    //Genera números aleatorios entre 0 y 1000.
-    rand() % 1000;
-
     //Bloqueo de semáforo para que  únicamnete un hilo entre en SC.
-    if(pthread_mutex_lock(&semaforo) != 0){printf("\nERROR. No se pudo bloquear el semáforo.\n");}
-    else{printf("\nEl semáforo se ha bloqueado.\n\n");}
+    if(pthread_mutex_lock(&semaforo_P) != 0){printf("\nERROR. No se pudo bloquear el semáforo productor.\n");}
+    else{printf("\nEl semáforo productor se ha bloqueado.\n\n");}
 
-    for (i = 0; i < ITER; i++) {
-	     l = counter;
-	     fprintf(stdout, "Hilo %d: %f\n", *id, counter);
-	     l++;
-	     counter = l;
+    for (i = 0; i < N; i++) {
+      //Genera números aleatorios entre 0 y 1000.
+      l = rand() % 1001;
+      buffer[i] = l;
+      suma_P += buffer[i];
+      fprintf(stdout, "Suma de Hilo Productor (id = %d): %f\n", *id, suma_P);
     }
 
     //Desbloqueo del semáforo al terminar la SC.
-    if (pthread_mutex_unlock(&semaforo) != 0) {printf("\nERROR. No se pudo desbloquear el semáforo.\n");}
-    else{printf("\nEl semáforo se ha desbloqueado.\n\n");}
+    if (pthread_mutex_unlock(&semaforo_P) != 0) {printf("\nERROR. No se pudo desbloquear el semáforo productor.\n");}
+    else{printf("\nEl semáforo productor se ha desbloqueado.\n\n");}
 
-    to_return = malloc(sizeof(double));
+    to_return_P = malloc(sizeof(double));
 
-    *to_return = counter;
+    *to_return_P = suma_P;
 
-    pthread_exit((void *) to_return);
+    pthread_exit((void *) to_return_P);
 }
 
-void *consumidor(void *p)
+//Hilo consumidor. -> Coge los números del buffer y los suma.
+void *consumidor(void *c)
 {
-    double l, *to_return;
-    extern double counter;
+    double l, *to_return_C;
+    extern double suma_C;
+    extern int buffer[N];
     int *id, i;
 
-    id = (int *) p;
+    id = (int *) c;
 
     //Guarda número aleatorios en un buffer.
 
     //Bloqueo de semáforo para que  únicamnete un hilo entre en SC.
-    if(pthread_mutex_lock(&semaforo) != 0){printf("\nERROR. No se pudo bloquear el semáforo.\n");}
-    else{printf("\nEl semáforo se ha bloqueado.\n\n");}
+    if(pthread_mutex_lock(&semaforo_C) != 0){printf("\nERROR. No se pudo bloquear el semáforo consumidor.\n");}
+    else{printf("\nEl semáforo consumidor se ha bloqueado.\n\n");}
 
-    for (i = 0; i < ITER; i++) {
-	     l = counter;
-	     fprintf(stdout, "Hilo %d: %f\n", *id, counter);
-	     l++;
-	     counter = l;
+    for (i = 0; i < N; i++) {
+      suma_C += buffer[i];
+	    fprintf(stdout, "Suma de Hilo Consumidor (id = %d): %f\n", *id, suma_C);
     }
 
     //Desbloqueo del semáforo al terminar la SC.
-    if (pthread_mutex_unlock(&semaforo) != 0) {printf("\nERROR. No se pudo desbloquear el semáforo.\n");}
-    else{printf("\nEl semáforo se ha desbloqueado.\n\n");}
+    if (pthread_mutex_unlock(&semaforo_C) != 0) {printf("\nERROR. No se pudo desbloquear el semáforo consumidor.\n");}
+    else{printf("\nEl semáforo consumidor se ha desbloqueado.\n\n");}
 
-    to_return = malloc(sizeof(double));
+    to_return_C = malloc(sizeof(double));
 
-    *to_return = counter;
+    *to_return_C = suma_C;
 
-    pthread_exit((void *) to_return);
+    pthread_exit((void *) to_return_C);
 }
