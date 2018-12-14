f501cc0a7b108b0d556a40ed244aa05cbe56f46f
Julen Pérez Hernández <i72pehej@uco.es>
2018-12-14 12:47:03 +0100

    Ideas para SO

--- SO/Practica2/Ejercicio3_2.c
+++ SO/Practica2/Ejercicio3_2.c
@@ -11,15 +11,16 @@ double suma_P = 0, suma_C = 0;
 //Inicialización de Semáforos.
 pthread_mutex_t semaforo_P, semaforo_C;
 
-#define N 5
+#define N 3
 #define N_Prod 15
 #define N_Cons 3
+#define LIMITE -1
 
 //Buffer/Vector de almacenamiento compartido.
 int buffer[N];
 
 int main(){
-  pthread_t hilo_P, hilo_C;
+  pthread_t hilo_P[N_Prod], hilo_C[N_Cons];
   int status, i, v_idProductor[N_Prod], v_idConsumidor[N_Cons];
   extern double suma_P, suma_C;
   void *productor(void *);
@@ -43,7 +44,7 @@ int main(){
   for (size_t p = 0; p < N_Prod; p++) {
     v_idProductor[p] = p;
 
-    if ((status = pthread_create(&hilo_P, NULL, productor, (void *) &v_idProductor[p])))
+    if ((status = pthread_create(&hilo_P[p], NULL, productor, (void *) &v_idProductor[p])))
     exit(status);
   }
 
@@ -51,15 +52,20 @@ int main(){
   for (size_t c = N_Prod, i = 0; c < (N_Prod + N_Cons), i < N_Cons; c++, i++) {
     v_idConsumidor[i] = c;
 
-    if ((status = pthread_create(&hilo_C, NULL, consumidor, (void *) &v_idConsumidor[i])))
+    if ((status = pthread_create(&hilo_C[i], NULL, consumidor, (void *) &v_idConsumidor[i])))
     exit(status);
   }
 
-  // Wait threads
-  pthread_join(hilo_P, (void **) &r_value_P);
-  printf("Value returned by %lu thread: %lf\n", hilo_P, *r_value_P);
-  pthread_join(hilo_C, (void **) &r_value_C);
-  printf("Value returned by %lu thread: %lf\n", hilo_C, *r_value_C);
+  //Espera de los hilos.
+  for (size_t p = 0; p < N_Prod; p++) {
+    pthread_join(hilo_P[p], (void **) &r_value_P);
+    printf("Valor devuelto por el hilo productor %lu: %lf\n", hilo_P[p], *r_value_P);
+  }
+
+  for (size_t c = 0; c < N_Cons; c++) {
+    pthread_join(hilo_C[c], (void **) &r_value_C);
+    printf("Valor devuelto por el hilo consumidor %lu: %lf\n", hilo_C[c], *r_value_C);
+  }
 
   //Destrucción de Semáforos.
   if (pthread_mutex_destroy(&semaforo_P) != 0) {
@@ -74,7 +80,7 @@ int main(){
   }
   else{printf("\nEl semáforo consumidor se ha destruido. Memoria liberada.\n");}
 
-  // Final result
+  //Resultado final.
   fprintf(stdout, "\nResultado final productor: %f\n", suma_P);
   fprintf(stdout, "Resultado final consumidor: %f\n\n", suma_C);
 
@@ -127,8 +133,13 @@ void *consumidor(void *c){
   else{printf("\nEl semáforo consumidor se ha bloqueado.\n\n");}
 
   for (i = 0; i < N; i++) {
-    suma_C += buffer[i];
-    fprintf(stdout, "Suma de Hilo Consumidor (id = %d): %f\n", *id, suma_C);
+    if (buffer[i] == NULL) {
+      fprintf(stdout, "Error al consumir. Buffer vacío.\n");
+    } else {
+      suma_C += buffer[i];
+      buffer[i];¿?¿?¿?¿ //Consumir el elemento del buffer.
+      fprintf(stdout, "Suma de Hilo Consumidor (id = %d): %f\n", *id, suma_C);
+    }
   }
 
   //Desbloqueo del semáforo al terminar la SC.
