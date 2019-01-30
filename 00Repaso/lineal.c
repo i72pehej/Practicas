d8f4dbdc83728a229b7d09f90e2c4e347df024d9
Julen Pérez Hernández <i72pehej@uco.es>
2019-01-30 23:21:57 +0100

    Rename

--- 00Repaso/lineal.c
+++ 00Repaso/lineal.c
@@ -1,15 +1,11 @@
 //Prácticando estructuras de procesos.
 
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
 #include <pthread.h>
-
-
-//Lineal/Secuencial.
 
 int main(int argc, char const *argv[]) {
   if (argc == 2) {
@@ -50,47 +46,3 @@ int main(int argc, char const *argv[]) {
     exit(EXIT_FAILURE);
   }
 }
-
-/*
-//Abanico.
-
-int main(int argc, char const *argv[]) {
-  if (argc == 2) {
-    int i, estado;
-    pid_t pid;
-
-    printf("\nFormato correcto.\n\n");
-
-    for (i = 0; i < atoi(argv[1]); i++) {
-      switch (fork()) {
-        case -1:  //ERROR.
-          printf("\nERROR. No se ha podido crear el proceso.\n");
-          exit(EXIT_FAILURE);
-
-        case 0:  //HIJO.
-          printf("\nSe ha creado el HIJO <%d>, con PADRE <%d>, correctamente.\n", getpid(), getppid());
-          exit(EXIT_SUCCESS);
-
-        break;
-      }
-    }
-
-    for(i = 0; i < atoi(argv[1]); i++) {
-      pid = wait(&estado);
-      if(pid == -1) {exit(EXIT_FAILURE);}
-      else {printf("\nProceso HIJO <%d>, con PADRE <%d>, terminado con estado: <%d>.\n", getpid(), getppid(), WEXITSTATUS(estado));}
-    }
-  }
-
-  else {
-    printf("\n¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡ERROR. Formato de comando incorrecto!!!!!!!!!!!!!!!!!!\n");
-    printf("\nTerminando ejecucuión . . .\n\n");
-    sleep(2);
-    exit(EXIT_FAILURE);
-  }
-
-  printf("\n\n");
-  printf("FIN. Ejecucuión terminada correctamente . . .\n\n");
-  exit(EXIT_SUCCESS);
-}
-*/
