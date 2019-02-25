50e25490edd0743f169f04523f9d375f862fa1b6
Julen Pérez Hernández <i72pehej@uco.es>
2019-02-25 19:37:59 +0100

    Prototipos Segunda Parte

--- ED/Practica1/operadoresExternosMonomios.cpp
+++ ED/Practica1/operadoresExternosMonomios.cpp
@@ -1,120 +1,252 @@
 /*!
    \file  operadoresExternosMonomios.cpp
-	\brief Ficheros con el código de los operadores externos de la clase Monomio
+	\brief Ficheros con el código de los operadores externos de la clase Monomio.
 */
 
 
-//  Ficheros de cabecera
+// Ficheros de cabecera.
 #include <iostream>
 
 #include "operadoresExternosMonomios.hpp"
 
 
-//  Se incluyen los operadores sobrecargados dentro del espacio de nombres ed
-namespace ed
-{
-	// Operadores de igualdad
+// Se incluyen los operadores sobrecargados dentro del espacio de nombres ed.
+namespace ed {
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+	// Operadores de igualdad.
 
 	// COMPLETAR
 
-	bool operator==(ed::Monomio const & m1, ed::Monomio const & m2)
-	{
+	// Comprueba si dos monomios son iguales.
+	bool operator == (ed::Monomio const &m1, ed::Monomio const &m2) {
 		// COMPLETAR Y MODIFICAR
 
 		// MODIFICAR: SE DEVUELVE UN VALOR ARBITRARIO PARA NO GENERAR AVISOS AL COMPILAR
 		return true;
 	}
 
-	// COMPLETAR LOS OTROS OPERADORES DE IGUALDAD
+
+	// Comprueba si un monomio es igual a un número real: "m == x".
+	bool operator == (ed::Monomio const &m, double const &x) {
+
+	}
 
 
+	// Comprueba si un número real es igual a un monomio: "x == m".
+	bool operator == (double const &x, ed::Monomio const &m) {
 
-	// Operadores de desigualdad
+	}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+	// Operadores de desigualdad.
 
 	// COMPLETAR
-	bool operator!=(ed::Monomio const & m1, ed::Monomio const & m2)
-	{
+
+	// Comprueba si dos monomios NO son iguales: "m1 != m2".
+	bool operator!=(ed::Monomio const &m1, ed::Monomio const &m2) {
 		// COMPLETAR Y MODIFICAR
 
 		// MODIFICAR: SE DEVUELVE UN VALOR ARBITRARIO PARA NO GENERAR AVISOS AL COMPILAR
 		return true;
 	}
 
-	// COMPLETAR LOS OTROS OPERADORES DE DESIGUALDAD
+
+	// Comprueba si un monomio NO es igual a un número real: "m != x".
+	bool operator != (ed::Monomio const &m, double const &x) {
+
+	}
 
 
-	////////////////////////////////////////////////////////////
+	// Comprueba si un número real NO es igual a un monomio: "x != m".
+	bool operator != (double const &x, ed::Monomio const &m) {
 
-	// Operadores unarios prefijos
+	}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+	// Operadores unarios prefijos.
 
 	// COMPLETAR
-	ed::Monomio & operator+(ed::Monomio const & m)
-	{
+	ed::Monomio &operator + (ed::Monomio const &m) {
+
 		// COMPLETAR Y MODIFICAR
-		// Se crea un nuevo objeto
+
+		// Se crea un nuevo objeto.
 		ed::Monomio *nuevo = new ed::Monomio();
 
-
-		// Se devuelve el resultado
-		return *nuevo;
-	}
-
-	// COMPLETAR EL OTRO OPERADOR UNARIO PREFIJO: resta
-
-
-	////////////////////////////////////////////////////////////
-	// Operadores aritméticos binarios
-
-	// Suma
-	ed::Monomio & operator+ (ed::Monomio const &m1, ed::Monomio const &m2)
-	{
-		// COMPLETAR Y MODIFICAR
-		// Se crea un nuevo objeto
-		ed::Monomio *nuevo = new ed::Monomio();
-
-
-		// Se devuelve el resultado
+		// Se devuelve el resultado.
 		return *nuevo;
 	}
 
 
-	////////////
+	ed::Monomio &operator - (ed::Monomio const &m) {
+
+		// COMPLETAR Y MODIFICAR
+
+		// Se crea un nuevo objeto.
+		ed::Monomio *nuevo = new ed::Monomio();
+
+		// Se devuelve el resultado.
+		return *nuevo;
+	}
+
+
+	// Operadores aritméticos binarios.
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+	// Suma.
+	ed::Monomio &operator + (ed::Monomio const &m1, ed::Monomio const &m2) {
+
+		// COMPLETAR Y MODIFICAR
+
+		// Se crea un nuevo objeto.
+		ed::Monomio *nuevo = new ed::Monomio();
+
+
+		// Se devuelve el resultado.
+		return *nuevo;
+	}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
 	// Resta
+	ed::Monomio &operator - (ed::Monomio const &m1, ed::Monomio const &m2) {
 
+		// COMPLETAR Y MODIFICAR
+
+		// Se crea un nuevo objeto.
+		ed::Monomio *nuevo = new ed::Monomio();
+
+
+		// Se devuelve el resultado.
+		return *nuevo;
+	}
 	// COMPLETAR
 
 
-	//////////////////
-	// Multiplicación
+////////////////////////////////////////////////////////////////////////////////
 
+
+	// Multiplicación.
+	ed::Monomio &operator * (ed::Monomio const &m1, ed::Monomio const &m2) {
+
+		// COMPLETAR Y MODIFICAR
+
+		// Se crea un nuevo objeto.
+		ed::Monomio *nuevo = new ed::Monomio();
+
+
+		// Se devuelve el resultado.
+		return *nuevo;
+	}
+
+
+	ed::Monomio &operator * (ed::Monomio const &m, double const &x) {
+
+		// COMPLETAR Y MODIFICAR
+
+		// Se crea un nuevo objeto.
+		ed::Monomio *nuevo = new ed::Monomio();
+
+
+		// Se devuelve el resultado.
+		return *nuevo;
+	}
+
+
+	ed::Monomio &operator * (double const &x, ed::Monomio const &m) {
+
+		// COMPLETAR Y MODIFICAR
+
+		// Se crea un nuevo objeto.
+		ed::Monomio *nuevo = new ed::Monomio();
+
+
+		// Se devuelve el resultado.
+		return *nuevo;
+	}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+	// División.
+	ed::Monomio &operator / (ed::Monomio const &m1, ed::Monomio const &m2) {
+
+		// COMPLETAR Y MODIFICAR
+
+		// Se crea un nuevo objeto.
+		ed::Monomio *nuevo = new ed::Monomio();
+
+
+		// Se devuelve el resultado.
+		return *nuevo;
+	}
+
+
+	ed::Monomio &operator / (ed::Monomio const &m, double const &x) {
+
+		// COMPLETAR Y MODIFICAR
+
+		// Se crea un nuevo objeto.
+		ed::Monomio *nuevo = new ed::Monomio();
+
+
+		// Se devuelve el resultado.
+		return *nuevo;
+	}
+
+
+	ed::Monomio &operator / (double const &x, ed::Monomio const &m) {
+
+		// COMPLETAR Y MODIFICAR
+
+		// Se crea un nuevo objeto.
+		ed::Monomio *nuevo = new ed::Monomio();
+
+
+		// Se devuelve el resultado.
+		return *nuevo;
+	}
 	// COMPLETAR
 
-	////////////
-	// División
 
-	// COMPLETAR
+////////////////////////////////////////////////////////////////////////////////
 
-
-	/////////////////////////////////////////////////////////////////////////////
 
 	//  Sobrecarga del operador de entrada
-	istream &operator>>(istream &stream, ed::Monomio &m)
-	{
+	istream &operator >> (istream &stream, ed::Monomio &m) {
 		// COMPLETAR
 
-   		// Se devuelve el flujo de entrada
+   		// Se devuelve el flujo de entrada.
 		return stream;
 	}
 
 
 	//  Sobrecarga del operador de salida
-	ostream &operator<<(ostream &stream, ed::Monomio const &m)
-	{
+	ostream &operator << (ostream &stream, ed::Monomio const &m) {
 		// COMPLETAR
 
-		// Se devuelve el flujo de salida
+		// Se devuelve el flujo de salida.
 		return stream;
 	}
+
+
+////////////////////////////////////////////////////////////////////////////////
 
 
 }  // namespace ed
--- ED/Practica1/operadoresExternosMonomios.hpp
+++ ED/Practica1/operadoresExternosMonomios.hpp
@@ -1,88 +1,120 @@
 /*!
 	\file operadoresExternosMonomios.hpp
-	\brief Ficheros con los prototipos de los operadores externos de la clase Monomio
+	\brief Ficheros con los prototipos de los operadores externos de la clase Monomio.
 */
 
 #ifndef _OperadoresExternosMonomios_HPP_
 #define _OperadoresExternosMonomios_HPP_
 
-// Facilita la entrada y salida
+// Facilita la entrada y salida.
 #include <iostream>
 
 #include "Monomio.hpp"
 
-// Para la sobrecarga de los operadores de flujo: << y >>
+// Para la sobrecarga de los operadores de flujo: << y >>.
 using std::istream;
 using std::ostream;
 
-// Espacio de nombre de la asigantura: ed
+// Espacio de nombre de la asigantura: ed.
 namespace ed
 {
-//////////////////////////////////////////////////////////////////////////////////////
-	// Operadores externos que no pertenecen a la clase Monomio
-
-	//! \name Operadores de igualdad
-
-	// COMPLETAR LOS COMENTARIOS DE DOXYGEN
-	bool operator==(ed::Monomio const & m1, ed::Monomio const & m2);
 
 
-	// COMPLETAR LOS OTROS OPERADORES DE IGUALDAD
+////////////////////////////////////////////////////////////////////////////////
 
 
-	//! \name Operadores de desigualdad
+	// Operadores externos que no pertenecen a la clase Monomio.
 
-	// COMPLETAR LOS COMENTARIOS DE DOXYGEN
-	bool operator!=(ed::Monomio const & m1, ed::Monomio const & m2);
-
-	// COMPLETAR LOS OTROS OPERADORES DE DESIGUALDAD
-
-	///////////////////////////////////////////////////
-
-	//! \name Operadores unarios prefijos
+	//! \name Operadores de igualdad.
 
 	// COMPLETAR LOS COMENTARIOS DE DOXYGEN
 
-	ed::Monomio & operator+(ed::Monomio const & m);
+	bool operator == (ed::Monomio const &m1, ed::Monomio const &m2);
 
-	// COMPLETAR EL OTRO OPERADOR UNARIO PREFIJO: resta
+	bool operator == (ed::Monomio const &m, double const &x);
 
-
-	//////////////////////////////////////////////////////////
-
-	//! \name Operador binario de la suma
-
-	// COMPLETAR LOS COMENTARIOS DE DOXYGEN
-	 ed::Monomio & operator+ (ed::Monomio const &m1, ed::Monomio const &m2);
+	bool operator == (double const &x, ed::Monomio const &m);
 
 
-	//////////////////////////////////////////////////////////
-	//! \name Operador binario de la resta
-
-	// COMPLETAR
+////////////////////////////////////////////////////////////////////////////////
 
 
-	///////////////////////////////////////////////////////////////////////////
-	//! \name Operadores binarios de la multiplicación
-
-	// COMPLETAR
-
-	///////////////////////////////////////////////////////////////////////////
-
-	//! \name Operadores binarios de la división
-
-	// COMPLETAR
-
-
-	/////////////////////////////////////////////////////////////////////////////
-
-	//! \name Sobrecarga de los operadores de entrada y salida
+	//! \name Operadores de desigualdad.
 
 	// COMPLETAR LOS COMENTARIOS DE DOXYGEN
-	 istream &operator>>(istream &stream, ed::Monomio &m);
+	bool operator != (ed::Monomio const &m1, ed::Monomio const &m2);
+
+	bool operator != (double const &x, ed::Monomio const &m);
+
+	bool operator != (ed::Monomio const &m, double const &x);
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+	//! \name Operadores unarios prefijos.
 
 	// COMPLETAR LOS COMENTARIOS DE DOXYGEN
-	 ostream &operator<<(ostream &stream, ed::Monomio const &m);
+
+	ed::Monomio &operator + (ed::Monomio const &m);
+
+	ed::Monomio &operator - (ed::Monomio const &m);
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+	//! \name Operador binario de la suma.
+
+	// COMPLETAR LOS COMENTARIOS DE DOXYGEN
+	 ed::Monomio &operator + (ed::Monomio const &m1, ed::Monomio const &m2);
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+	//! \name Operador binario de la resta.
+
+	ed::Monomio &operator - (ed::Monomio const &m1, ed::Monomio const &m2);
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+	//! \name Operadores binarios de la multiplicación.
+
+	ed::Monomio &operator * (ed::Monomio const &m1, ed::Monomio const &m2);
+
+	ed::Monomio &operator * (ed::Monomio const &m, double const &x);
+
+	ed::Monomio &operator * (double const &x, ed::Monomio const &m);
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+	//! \name Operadores binarios de la división.
+
+	ed::Monomio &operator / (ed::Monomio const &m1, ed::Monomio const &m2);
+
+	ed::Monomio &operator / (ed::Monomio const &m, double const &x);
+
+	ed::Monomio &operator / (double const &x, ed::Monomio const &m);
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+	//! \name Sobrecarga de los operadores de entrada y salida.
+
+	// COMPLETAR LOS COMENTARIOS DE DOXYGEN
+	 istream &operator >> (istream &stream, ed::Monomio &m);
+
+	// COMPLETAR LOS COMENTARIOS DE DOXYGEN
+	 ostream &operator << (ostream &stream, ed::Monomio const &m);
+
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 }  // Fin de namespace ed.
 
