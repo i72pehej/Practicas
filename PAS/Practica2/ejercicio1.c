3f3f59f8f874d30c1a09322467e198b4365b9f77
Julen Pérez Hernández <i72pehej@uco.es>
2019-03-19 00:36:26 +0100

    A

--- PAS/Practica2/ejercicio1.c
+++ PAS/Practica2/ejercicio1.c
@@ -16,19 +16,21 @@ argumentos.
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
-#include <stdio.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <pwd.h>
 #include <grp.h>
-#include <unistd.h>
 
 
 int main (int argc, char **argv) {
+	int opcion;
+
 	char *lgn;
 	struct passwd *pw;
-	struct grupo *gr;
+	struct group *gr;
 
-	int opcion;
+	FILE *f_grupo;
+	char *info_grupo;
 
 	/* Estructura a utilizar por getoptlong */
 	static struct option long_options[] = {
@@ -38,52 +40,126 @@ int main (int argc, char **argv) {
 		{"useruid",   required_argument, 0, 'i'},
 		{"groupname", required_argument, 0, 'g'},
 		{"groupuid",  required_argument, 0, 'd'},
-		{"allgroups", required_argument, 0, 's'},
+		{"allgroups", no_argument,			 0, 's'},
 		/* Necesario para indicar el final de las opciones */
 		{0, 0, 0, 0}
 	};
 
 	/* Estas variables servirán para almacenar el resultado de procesar la línea de comandos */
-	char *u_value = NULL;
-	char *i_value = NULL;
-	char *g_value = NULL;
-	char *d_value = NULL;
-	char *s_value = NULL;
+	char *u_value = NULL;	//Nombre de usuario del sistema.
+	char *i_value = NULL;	//Identificador de usuario del sistema.
+	char *g_value = NULL;	//Nombre de un grupo del sistema.
+	char *d_value = NULL;	//Identificador de un grupo del sistema.
+	int 	s_value = 0;	//Información de todos los grupos.
 
 	/* getopt_long guardará el índice de la opción en esta variable. */
 	int option_index = 0;
 
 	/* Deshabilitar la impresión de errores por defecto */
-	/* opterr=0; */
-	while ((opcion = getopt_long(argc, argv, "u:i:g:d:s:", long_options, &option_index)) != -1) {
+	opterr = 0;
+
+	while ((opcion = getopt_long(argc, argv, "u:i:g:d:s", long_options, &option_index)) != -1) {
 		/*El usuario ha terminado de introducir opciones.*/
 		if (opcion == -1) {break;}
 
 		switch (opcion) {
+			// Muestra la información de "passwd" de un usuario mediante su nombre de usuario.
 			case 'u':
 				printf("\nOpción -u con valor '%s'\n", optarg);
 				u_value = optarg;
-				break;
 
+				if ((lgn = u_value /*getenv("USER")*/) == NULL || (pw = getpwnam(lgn)) == NULL) {
+          fprintf(stderr, "\nError al obtener información de usuario.\n");
+          exit(1);
+        }
+
+				//Aqui ya se dispone de la estructura que contiene informacion del usuario.
+				printf("\n");
+				printf("\tNombre de Usuario: %s\n", 		pw->pw_gecos);
+				printf("\tLogin: %s\n", 								pw->pw_name);
+				printf("\tPassword: %s\n", 							pw->pw_passwd);
+				printf("\tUID: %d\n", 									pw->pw_uid);
+				printf("\tDirectorio HOME: %s\n", 			pw->pw_dir);
+				printf("\tID de grupo principal: %d\n", pw->pw_gid);
+				printf("\n");
+
+			break;
+
+			// Muestra la información de "passwd" de un usuario mediante su ID de usuario.
 			case 'i':
 				printf("\nOpción -i con valor '%s'\n", optarg);
 				i_value = optarg;
-				break;
 
+				if ((lgn = i_value /*getenv("USER")*/) == NULL || (pw = getpwuid(atoi(lgn))) == NULL) {
+          fprintf(stderr, "\nError al obtener información de usuario.\n");
+          exit(1);
+        }
+
+				//Aqui ya se dispone de la estructura que contiene informacion del usuario.
+				printf("\n");
+				printf("\tNombre de Usuario: %s\n", 		pw->pw_gecos);
+				printf("\tLogin: %s\n", 								pw->pw_name);
+				printf("\tPassword: %s\n", 							pw->pw_passwd);
+				printf("\tUID: %d\n", 									pw->pw_uid);
+				printf("\tDirectorio HOME: %s\n", 			pw->pw_dir);
+				printf("\tID de grupo principal: %d\n", pw->pw_gid);
+				printf("\n");
+
+			break;
+
+			// Muestra el GID de "group" de un grupo mediante su nombre de grupo.
 			case 'g':
 				printf("\nOpción -g con valor '%s'\n", optarg);
 				g_value = optarg;
-				break;
 
+				if ((lgn = g_value /*getenv("USER")*/) == NULL || (gr = getgrnam(lgn)) == NULL) {
+          fprintf(stderr, "\nError al obtener información del grupo.\n");
+          exit(1);
+        }
+
+				printf("\n");
+			 	// printf("\tNombre del grupo principal: %s\n", gr->gr_name);
+				printf("\tID del grupo principal: %d\n", 		 gr->gr_gid);
+				printf("\n");
+
+			break;
+
+			// Muestra el nombre de "group" de un grupo mediante su GID de grupo.
 			case 'd':
 				printf("\nOpción -d con valor '%s'\n", optarg);
 				d_value = optarg;
-				break;
 
+				if ((lgn = d_value /*getenv("USER")*/) == NULL || (gr = getgrgid(atoi(lgn))) == NULL) {
+          fprintf(stderr, "\nError al obtener información del grupo.\n");
+          exit(1);
+        }
+
+				printf("\n");
+				printf("\tNombre del grupo principal: %s\n", gr->gr_name);
+				// printf("\tID del grupo principal: %s\n", 		 gr->gr_gid);
+				printf("\n");
+
+			break;
+
+			// Muestra la información de todos los grupos del sistema.
 			case 's':
-				printf("\nOpción -s con valor '%s'\n", optarg);
-				s_value = optarg;
-				break;
+				s_value = 1;
+
+				//Apertura del fichero de información de grupos: "/etc/group".
+				if(f_grupo = fopen("group", "r")) {
+					printf("ERROR. Imposible abrir fichero.\n");
+					exit(1);
+				}
+				else {printf("El fichero se abrió correctamente.\n");}
+
+				printf("\n");
+				while (fgets(info_grupo, 30, f_grupo) != NULL) {
+					printf("Información de grupo: %s\n", info_grupo);
+				}
+				printf("\n");
+
+				fclose(f_grupo);
+			break;
 
 			case '?':
 				/* getopt_long ya imprime su mensaje de error, no es necesario hacer nada.*/
@@ -95,10 +171,12 @@ int main (int argc, char **argv) {
 				if (optopt == 'd') {fprintf (stderr, "La opción %c requiere un argumento.\n", optopt);}
 				if (optopt == 's') {fprintf (stderr, "La opción %c requiere un argumento.\n", optopt);}
 
-				break;
+			break;
 				// */
 
 			default:
+				printf("\n\nERROR FATAL.\n\n");
+				printf("\n\nAbortando . . .\n\n");
 				abort ();
 		}
 		printf("optind: %d, optarg: %s, optopt: %c\n", optind, optarg, optopt);
@@ -112,7 +190,7 @@ int main (int argc, char **argv) {
 		putchar ('\n');
 	}
 
-	printf ("\nu_value = %s, i_value = %s, g_value = %s, d_value = %s, s_value = %s.\n",u_value, i_value, g_value, d_value, s_value);
+	printf ("\nu_value = %s, i_value = %s, g_value = %s, d_value = %s, s_value = %d.\n",u_value, i_value, g_value, d_value, s_value);
 
 	exit (0);
 }
---
+++ PAS/Practica2/ejercicio2.c
@@ -0,0 +1,15 @@
+/*ejercicio2.c:
+Cuando la comunicación entre procesos es simple y ambos extremos de la comunicación han
+sido generados con un fork() o pertenecen al mismo proceso, se pueden utilizar tuberías
+anónimas (pipes).
+
+Las tuberías son unidireccionales, por lo que para hacer una comunicación full-duplex
+se necesitarían dos tuberías. Implemente un programa en C que cree un proceso hijo usando
+la primitiva fork(), de forma que el padre y el hijo se comunicarán mediante dos tuberías.
+*/
+
+
+int main(int argc, char const *argv[]) {
+  
+  return 0;
+}
