db7435c1bfbe7c1cfd45e0905b905256c1fa3b9e
Julen Pérez Hernández <i72pehej@uco.es>
2019-03-19 00:34:48 +0100

    Casa

--- MP/Otros/jaja.c
+++ MP/Otros/jaja.c
@@ -2,10 +2,16 @@
 #include <stdlib.h>
 
 int main(int argc, char const *argv[]) {
-  float **tabla;// = {{1, 2, 3}, {4, 5, 6}};
 
-//  printf("Valores de tabla: %p, (tabla+1): %p, *(tabla+1): %p, (*(tabla+1)+1): %p, (*(tabla)+1): %p.\n", (void*)tabla, (void*)(tabla+1), (void*)*(tabla+1), (void*)(*(tabla+1)+1), (void*)(*(tabla)+1));
-  printf("Valores de *(*(tabla+1)+1): %f, *(*(tabla)+1): %f, *(*(tabla+1)): %f.\n", *(*(tabla+1)+1), *(*(tabla)+1), *(*(tabla+1)));
+  int **m;
+  int i, j;
+  i = j = 1;
+
+  m[i][j] = 1;
+  int valor = m[i][j];
+
+  printf("Valor i j: %d\n", valor);
+
 
   return 0;
 }
